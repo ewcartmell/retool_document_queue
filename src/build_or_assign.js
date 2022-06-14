80954a4d1e9554c3d37458b00d23820a08ce0815
Ned Cartmell <ned@flexport.com>
2022-06-13 17:00:43 -0700

    Update build_or_assign.js

--- src/build_or_assign.js
+++ src/build_or_assign.js
@@ -1,6 +1,7 @@
 window.build_or_assign = function build_or_assign(arr = []) {
   arr.forEach(function (item, index) {
-  var doc = Document.get_document(item.GRAPHQL_DOCUMENT_ID)
+  var doc_id = (item.DOCUMENT_ID ?? item.GSHEET_DOCUMENT_ID) ?? item.GRAPHQL_DOCUMENT_ID;
+  var doc = Document.get_document(doc_id)
   if(doc == null) {
     new Document(item)
   } else {
