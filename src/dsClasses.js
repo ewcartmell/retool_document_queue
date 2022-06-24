34a9dca78f071833c63f3580ec3f6cec60c63362
Ned Cartmell <ned@flexport.com>
2022-06-24 13:09:32 -0700

    convert ghseet_document_id to int to allow for matching against snowflake doc id

--- src/dsClasses.js
+++ src/dsClasses.js
@@ -335,7 +335,7 @@ window.Coordinator = class Coordinator {
 
 window.build_or_assign = function build_or_assign(arr = []) {
   arr.forEach(function (item, index) {
-    var doc_id = (item.DOCUMENT_ID ?? item.GSHEET_DOCUMENT_ID) ?? item.GRAPHQL_DOCUMENT_ID;
+    var doc_id = (item.DOCUMENT_ID ?? parseInt(item.GSHEET_DOCUMENT_ID) ?? item.GRAPHQL_DOCUMENT_ID;
     var doc = Document.get_document(doc_id)
     if(!(doc == null) && doc.hasOwnProperty('ARRIVAL_DATE')) {
       item.ARRIVAL_DATE = new Date(item.ARRIVAL_DATE);
