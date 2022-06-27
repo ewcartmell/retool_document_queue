463c943d80c3a56d518d02d01d35ee12ccd625e1
Ned Cartmell <ned@flexport.com>
2022-06-27 12:22:07 -0700

    update build or assign with actioned doc

--- src/dsClasses.js
+++ src/dsClasses.js
@@ -341,7 +341,7 @@ window.Coordinator = class Coordinator {
 
 window.build_or_assign = function build_or_assign(arr = []) {
   arr.forEach(function (item, index) {
-    var doc_id = parseInt(item.DOCUMENT_ID ?? item.GSHEET_DOCUMENT_ID) ?? item.GRAPHQL_DOCUMENT_ID;
+    var doc_id = (parseInt(item.DOCUMENT_ID ?? item.GSHEET_DOCUMENT_ID) ?? item.GRAPHQL_DOCUMENT_ID) ?? parseInt(item.ACTIONED_DOCUMENT_ID);
     var doc = Document.get_document(doc_id)
 
     if(doc == null) {
