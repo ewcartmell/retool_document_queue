c543df11c9d5d780d311dbbff246053864487d3c
Ned Cartmell <ned@flexport.com>
2022-07-10 14:04:55 -0700

    Eliminate requirement to be execution

--- src/dsClasses.js
+++ src/dsClasses.js
@@ -272,7 +272,6 @@ window.Coordinator = class Coordinator {
     var coordinators = arr.filter(c =>
       c.TEAM == "Product Data"
       && COMPANY.includes(c.COMPANY)
-      && c.ROLE == "Execution"
     )
 
     var results = coordinators.map(c => ({
